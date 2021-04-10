312a758ea97e9995e3e0d74d99bdda7133ec5d78
Hoang Lam <meolam1999@gmail.com>
2021-04-10 18:41:25 +0000

    convert to function

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,8 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import QUESTION_DATA from '../data/quiz-data.json';
+import useQuestionStorage from '../hooks/useQuestionStorage';
 import Question from './Question';
 
-const QuestionList = ({ questions, handleAnswerClick, handleEnterPress }) => {
+function QuestionList() {
+  const totalQuestions = 10;
+  const [questions, checkAnswer] = useQuestionStorage(QUESTION_DATA, totalQuestions);
+  
+  const handleAnswerClick = (index) => (e) => {
+    const { questions, step, userAnswers } = this.state;
+    const isCorrect = questions[0].correct === index;
+    const currentStep = step - 1;
+    const tries = userAnswers[currentStep].tries;
+
+    if (isCorrect && e.target.nodeName === 'LI') {
+      // Prevent other answers from being clicked after correct answer is clicked
+      e.target.parentNode.style.pointerEvents = 'none';
+
+      e.target.classList.add('right');
+
+      userAnswers[currentStep] = {
+        tries: tries + 1
+      };
+
+      this.setState({
+        userAnswers: userAnswers
+      });
+
+      setTimeout(() => this.showModal(tries), 750);
+
+      setTimeout(this.nextStep, 2750);
+    }
+
+    else if (e.target.nodeName === 'LI') {
+      e.target.style.pointerEvents = 'none';
+      e.target.classList.add('wrong');
+
+      userAnswers[currentStep] = {
+        tries: tries + 1
+      };
+
+      this.setState({
+        userAnswers: userAnswers
+      });
+    }
+  };
+
+  const handleEnterPress = (index) => (e) => {
+    if (e.keyCode === 13) {
+      this.handleAnswerClick(index)(e);
+    }
+  };
+  
   return (
     <ul className="question-list">
       {questions.map(question => {
@@ -18,12 +68,6 @@ const QuestionList = ({ questions, handleAnswerClick, handleEnterPress }) => {
       })}
     </ul>
   );
-};
-
-QuestionList.propTypes = {
-  questions: PropTypes.array.isRequired,
-  handleAnswerClick: PropTypes.func.isRequired,
-  handleEnterPress: PropTypes.func.isRequired
-};
+}
 
 export default QuestionList;
